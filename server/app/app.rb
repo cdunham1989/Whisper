e06c32a97ebeb0bd4c03f34be006723daf7e8e9f
oliverpople <oliverpople@gmail.com>
2017-12-06 17:36:13 +0000

    Attempts to commit changes

--- server/app/app.rb
+++ server/app/app.rb
@@ -28,7 +28,7 @@ EventMachine.run do
 
   @clients = []
 
-  EM::WebSocket.start(:host => '0.0.0.0', :port => '9292') do |ws|
+  EM::WebSocket.start(:host => '0.0.0.0', :port => '3001') do |ws|
     ws.onopen do |handshake|
       @clients << ws
       ws.send "Connected to #{handshake.path}"
