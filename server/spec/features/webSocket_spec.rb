d6ff78cdd354a2333b228c6aad52c35ad5242238
oliverpople <oliverpople@gmail.com>
2017-12-07 08:27:20 +0000

    Added first draft of webSocket_spec tests expectation statements.

--- server/spec/features/webSocket_spec.rb
+++ server/spec/features/webSocket_spec.rb
@@ -11,13 +11,21 @@ require(File.expand_path('../../app/public/javascript/webSocket.js', File.dirnam
 'should have an onopen method'
 'should have an onclose method'
 'should have an onmessage method'
-'it should intialise connection between client and websocket'
-        'view has content "Socket State: 0"
-        "view has content "Socket State: 1 (open)"
-        "view content "Received: Connected to /"
 
-"it should disconnect client and websocket when the user decides"
-          "view has content "Socket State: 3 (close)"
+'it should attempt to intialise connection between client and websocket'
+        'view has content "Socket State: 0" / "socket.readyState = 0 i.e. connection is yet to be established'
+
+'it should establish to intialise connection between client and websocket'
+        "view has content 'Socket State: 1 (open)'" / "socket.readyState = 1 i.e. connection has been established and messages can be exchanged between the client and the server"
+
+'it should attempt to close/ disconnect to the connection between client and websocket'
+        "socket.readyState = 2 i.e. The connection is going through the closing handshake."
+
+'it should close/ disconnect to the connection between client and websocket'
+        "view has content 'Socket State: 3 (close)'" / "socket.readyState = 3 i.e. The connection has been closed or could not be opened.'
+
+'it should intialise the apps index view'
+  "view content 'Received: Connected to /'"
 
 "it takes the user\'s message string and appends it to the view content"
 
