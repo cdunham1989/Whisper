761a94788d818e8e1dca3c589d0f20bc0ea22e07
hugosykes <hugo@denmeadfarm.co.uk>
2017-12-07 12:47:53 +0000

    Updated gitignore and cleaned out some superfluous tests

--- server/spec/features/webSocket_spec.rb
+++ server/spec/features/webSocket_spec.rb
@@ -1,6 +1,5 @@
 require(File.expand_path('../spec_helper', File.dirname(__FILE__)))
 
-
 feature 'WebSocket feature' do
   scenario 'it should attempt to intialise connection between client and websocket' do
     visit('/')
@@ -12,38 +11,37 @@ feature 'WebSocket feature' do
     expect(page).to have_content('Socket State: 1 (open)')
   end
 
-  scenario 'it should close/ disconnect to the connection between client and websocket' do
+  scenario 'it should close the connection between client and websocket' do
     visit('/')
     expect(page).to have_content('Socket State: 3 (close)')
   end
 
-  scenario 'it should intialise the apps index view' do
+  scenario 'it should initialise the apps index view' do
     visit('/')
     expect(page).to have_content('Received: Connected to /')
   end
 
-  scenario "it takes the user's message string and appends it to the view content" do
+  scenario "it should take the user's message string and append it to the view content" do
     visit('/')
     fill_in :message, with: 'hello\n'
     expect(page).to have_content('hello')
   end
 
-  scenario "it throws error when form input 'message' is an empty string" do
+  scenario "it should throw an error when form input 'message' is an empty string" do
     visit('/')
     fill_in :message, with: 'hello\n'
     expect(page).to have_content('Please Enter a Message')
   end
 
-  scenario "it confirms message has been recieved by server" do
+  scenario "it should confirm that message has been recieved by server" do
     visit('/')
     fill_in :message, with: 'hello\n'
     expect(page).to have_content('Received: hello')
   end
 
-  scenario "it confirms message has been sent to server" do
+  scenario "it should confirm that message has been sent to server" do
     visit('/')
     fill_in :message, with: 'hello\n'
     expect(page).to have_content('Sent: hello')
   end
-
 end
