be9551417debccd4476b11c8d8073dafa4bde03d
Lucy Borthwick <lucyborthwick95@gmail.com>
2017-12-08 16:11:03 +0000

    JSQ messages view controller has black bubbles

--- Whisper/Whisper/Controller/MessageViewController.swift
+++ Whisper/Whisper/Controller/MessageViewController.swift
@@ -12,24 +12,67 @@ import JSQMessagesViewController
 class MessageViewController: JSQMessagesViewController {
     
     private var messages = [JSQMessage]();
+    
+    lazy var outgoingBubble: JSQMessagesBubbleImage = {
+        return JSQMessagesBubbleImageFactory()!.outgoingMessagesBubbleImage(with: UIColor.black)
+    }()
+    
+    
+    lazy var incomingBubble: JSQMessagesBubbleImage = {
+        return JSQMessagesBubbleImageFactory()!.incomingMessagesBubbleImage(with: UIColor.black)
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.senderId = "1"
         self.senderDisplayName = "Lucy";
+        
+        inputToolbar.contentView.leftBarButtonItem = nil
+        collectionView.collectionViewLayout.incomingAvatarViewSize = CGSize.zero
+        collectionView.collectionViewLayout.outgoingAvatarViewSize = CGSize.zero
     }
+    
+    // COLLECTION VIEW FUNCTIONS
     
     override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
         return messages[indexPath.item]
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return messages.count;
+        return messages.count
     }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAt indexPath: IndexPath!) -> JSQMessageBubbleImageDataSource! {
+        return messages[indexPath.item].senderId == senderId ? outgoingBubble : incomingBubble
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAt indexPath: IndexPath!) -> JSQMessageAvatarImageDataSource! {
+        return nil
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, attributedTextForMessageBubbleTopLabelAt indexPath: IndexPath!) -> NSAttributedString! {
+        return messages[indexPath.item].senderId == senderId ? nil : NSAttributedString(string: messages[indexPath.item].senderDisplayName)
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, layout collectionViewLayout: JSQMessagesCollectionViewFlowLayout!, heightForMessageBubbleTopLabelAt indexPath: IndexPath!) -> CGFloat {
+        return messages[indexPath.item].senderId == senderId ? 0 : 15
+    }
+    
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = super.collectionView(collectionView, cellForItemAt: indexPath) as! JSQMessagesCollectionViewCell
         return cell;
+    }
+    
+    // END COLLECTION VIEW FUNCTIONS
+    
+    override func didPressSend(_ button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: Date!) {
+        messages.append(JSQMessage(senderId: senderId, displayName: senderDisplayName, text: text));
+        collectionView.reloadData()
+        
+        
+        finishSendingMessage();
     }
     
     @IBAction func backButton(_ sender: Any) {
