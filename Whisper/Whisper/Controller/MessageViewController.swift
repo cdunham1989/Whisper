a48fa698dfea3e623bb6324a33dba562d473a1c6
Lucy Borthwick <lucyborthwick95@gmail.com>
2017-12-08 14:53:04 +0000

    JSQmessages controller initial set up

--- Whisper/Whisper/Controller/MessageViewController.swift
+++ Whisper/Whisper/Controller/MessageViewController.swift
@@ -7,15 +7,30 @@
 //
 
 import UIKit
+import JSQMessagesViewController
 
-class MessageViewController: UIViewController {
+class MessageViewController: JSQMessagesViewController {
+    
+    private var messages = [JSQMessage]();
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.senderId = "1"
+        self.senderDisplayName = "Lucy";
     }
-
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
+        return messages[indexPath.item]
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count;
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = super.collectionView(collectionView, cellForItemAt: indexPath) as! JSQMessagesCollectionViewCell
+        return cell;
+    }
     
     @IBAction func backButton(_ sender: Any) {
         dismiss(animated: true, completion: nil);
