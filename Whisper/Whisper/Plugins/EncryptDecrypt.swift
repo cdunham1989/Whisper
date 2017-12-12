e71a1b901e93b257fa09e3ad7cd9806627f18419
Lucy Borthwick <lucyborthwick95@gmail.com>
2017-12-12 17:03:41 +0000

    Adding decryption funciton

--- Whisper/Whisper/Plugins/EncryptDecrypt.swift
+++ Whisper/Whisper/Plugins/EncryptDecrypt.swift
@@ -30,6 +30,18 @@ class EncryptDecrypt {
     }
     
 
+        func decryptPressed(password: String) -> String {
+            let input = password
+            let key = keyClass.key
+            let iv = "gqLOHUioQ0QjhuvI"
+            let des = try! input.aesDecrypt(key, iv: iv)
+            return des
+    
+        }
+    
+    
+
+    
 }
     
 extension String{
@@ -51,7 +63,28 @@ extension String{
         
         return result
     }
-}
+    
+        func aesDecrypt(_ key: String, iv: String) throws -> String {
+            var result = ""
+            let keyClass = Key()
+            do {
+    
+                let hexString = self
+                let key: [UInt8] = Array(keyClass.key.utf8) as [UInt8]
+    
+                let aes = try! AES(key: key, blockMode: .ECB, padding: .pkcs7) //AES128 .ECB pkcs7
+                let decrypted = try aes.decrypt(Array(hex: hexString))
+    
+                result = String(data: Data(decrypted), encoding: .utf8) ?? ""
+    
+            } catch {
+                print(error)
+            }
+                return result
+            }
+     }
+    
+
 
 
 
