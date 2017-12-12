16e8e19bf562a5af19db1d0d56b30f8ca9c67ece
Lucy Borthwick <lucyborthwick95@gmail.com>
2017-12-12 15:34:52 +0000

    Passwords are encrypted on sign in and verified on login!

--- Whisper/Whisper/Controller/SignInViewController.swift
+++ Whisper/Whisper/Controller/SignInViewController.swift
@@ -36,7 +36,9 @@ class SignInViewController: UIViewController {
         
         if UsernameTextField.text != "" && PasswordTextField.text != "" {
             
-            AuthProvider.Instance.login(email: UsernameTextField.text!, password: PasswordTextField.text!, loginHandler: { ( message) in
+        let encryptedPassword = EncryptDecrypt.Instance.encryptPressed(password: PasswordTextField.text!)
+
+            AuthProvider.Instance.login(email: UsernameTextField.text!, password: encryptedPassword, loginHandler: { ( message) in
                     if message != nil {
                         self.alertTheUser(title: "Problem with authentication", message: message!);
                     } else {
