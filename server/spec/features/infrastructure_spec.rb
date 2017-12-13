ba4bdb63048128943a70ba77bdd016dd4c565808
Lucy Borthwick <lucyborthwick95@gmail.com>
2017-12-13 18:56:57 +0000

    Removing unused server file - will be replaced with link to server repo in readme

--- server/spec/features/infrastructure_spec.rb
+++
@@ -1,8 +0,0 @@
-require(File.expand_path('../spec_helper', File.dirname(__FILE__)))
-
-feature 'Infrastructure' do 
-  scenario 'going to home page' do
-    visit('/')
-    expect(current_url).to eq('http://www.example.com/')
-  end
-end
